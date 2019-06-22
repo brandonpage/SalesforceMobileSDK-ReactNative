d8f106b042e449ccd0b2a01c077df1fb1f89ae2e
brandonpage <bpage1@salesforce.com>
2019-06-21 17:37:01 -0700

    test more stuff

--- ios/SalesforceReactTests/prepareios.js
+++ ios/SalesforceReactTests/prepareios.js
@@ -9,7 +9,7 @@ console.log('=== Installing sdk dependencies');
 execSync('node ./updatesdk.js', {stdio: [0,1,2]});
 
 console.log('=== Installing pod dependencies');
-execSync('pod update', {stdio:[0,1,2]});
+execSync('pod install', {stdio:[0,1,2]});
 
 console.log('=== Creating test_credentials.json');
 execSync('touch test_credentials.json', {stdio:[0,1,2]});
